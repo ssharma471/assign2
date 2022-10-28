8282833a2e5fc71e2bdfed9e8ab6d5a00a3a84d3
sidhantss <sidhantsharmavlog@gmail.com>
2022-10-28 17:15:46 -0400

    updated

--- data-service.js
+++ data-service.js
@@ -1,14 +1,12 @@
 //Name: Sidhant Sharma Student ID: 123151219 Date: 04, October 2022
-
+// One of my classmates Ujjwal and my brother helped me for recalling the concepts and made me understand the things in a better way 
 const fp = require('fs');
 let Emp = [], Dep = [], Man = [];
 
 module.exports.initialize = function () {
-  return new Promise((resolve, reject) => 
-  {
+  return new Promise((resolve, reject) => {
     fp.readFile('./data/employees.json', (err, data) => {
-      if (err) 
-      {
+      if (err) {
         reject("unable to read file");
       }
       Emp = JSON.parse(data);
@@ -16,8 +14,7 @@ module.exports.initialize = function () {
     });
 
     fp.readFile('./data/departments.json', (err, data) => {
-      if (err) 
-      {
+      if (err) {
         reject("unable to read file");
       }
       Dep = JSON.parse(data);
@@ -31,7 +28,7 @@ module.exports.getAllEmployees = function () {
     if (Emp.length > 0) {
 
       resolve(Emp);
-   
+
     }
     reject('No Employee Found!');
 
@@ -44,16 +41,16 @@ module.exports.getAllManagers = function () {
   return new Promise((resolve, reject) => {
 
     for (let i = 0; i < Emp.length; i++) {
-      if (Emp[i].isManager == true) 
-       Man.push(Emp[i]);
+      if (Emp[i].isManager == true)
+        Man.push(Emp[i]);
 
-      
+
     }
 
     if (Man.length > 0) {
       resolve(Emp);
     }
-    
+
     reject('No Managers Found!');
 
   });
@@ -67,8 +64,89 @@ module.exports.getAllDepartments = function () {
       resolve(Dep);
 
     }
-
-
     reject('No Department Found!');
   });
 };
+
+module.exports.addEmployee = function (employeeData) {
+  return new Promise((resolve, reject) => {
+    if (employeeData.isManager == undefined) {
+      employeeData.isManager = false;
+    }
+    else {
+      employeeData.isManager = true;
+    }
+    employeeData.employeeNum = Emp.length + 1;
+    Emp.push(employeeData);
+    resolve(Emp);
+  });
+}
+
+module.exports.getEmployeesByStatus = function(status) {
+  return new Promise((resolve, reject)=> {
+    let vals = [];
+    for (let i= 0; i < Emp.length; i++) {
+      if (Emp[i].status == status) {
+        vals.push(Emp[i]);
+      }
+    }
+  if (Emp.length == 0) {
+    reject("no results returned");
+  }
+  resolve(vals);
+  });
+}
+
+module.exports.getEmployeesByDepartment = function(department) {
+  return new Promise((resolve, reject)=> {
+    let vals = [];
+    for (let i= 0; i < Emp.length; i++) {
+      if (Emp[i].department == department) {
+        vals.push(Emp[i]);
+      }
+    }
+  
+  if (employees.length == 0) 
+  {
+    reject("no results returned");
+  }
+  resolve(vals);
+  });
+
+}
+
+module.exports.getEmployeesByManager = function(manager) {
+  return new Promise((resolve, reject)=> {
+    let vals = [];
+    for (let i= 0; i < Emp.length; i++) {
+      if (Emp[i].employeeManagerNum == manager) {
+        vals.push(Emp[i]);
+      }
+    }
+  
+  if (Emp.length == 0) {
+    reject("no results returned");
+  }
+  resolve(vals);
+    
+  });
+
+}
+
+module.exports.getEmployeeByNum = function(num) {
+  return new Promise((resolve, reject)=> {
+    let vals = [];
+    for (let i= 0; i < Emp.length; i++) {
+      if (Emp[i].employeeNum == num) {
+        vals.push(Emp[i]);
+      }
+    }
+  
+  if (Emp.length == 0) {
+    reject("no results returned");
+  }
+  resolve(vals);
+    
+  });
+
+}
--- server.js
+++ server.js
@@ -7,9 +7,10 @@ other source
 *
 * Name: Sidhant Sharma Student ID: 123151219 Date: 04, October 2022
 *
-* Your app’s URL (from Cyclic) : ______________________________________________
-*
-*************************************************************************/ 
+* Your app’s URL (from Cyclic) : https://fierce-tuna-threads.cyclic.app/
+*One of my classmates Ujjwal and my brother helped me for recalling the concepts and made me understand the things in a better way 
+
+*************************************************************************/
 var express = require("express");
 var path = require("path");
 var dataservice = require('./data-service.js');
@@ -17,43 +18,127 @@ var app = express();
 var HTTP_PORT = process.env.PORT || 8080;
 app.use(express.static('public'));
 
-function onHTTPStart(){
-    console.log('Express http server listening on '+ HTTP_PORT);
+var body_Parser = require('body-parser');
+const multer = require("multer");
+const fs = require('fs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(body_Parser.urlencoded({ extended: true }));
+
+function onHTTPStart() {
+  console.log('Express http server listening on ' + HTTP_PORT);
 }
 
-app.get("/", function(req, res)  {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+// ADDING IMAGES
+const storage = multer.diskStorage({
+  destination: "./public/images/uploaded",
+  filename: function (req, file, cb) {
+
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage });
+
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.get('/about', function(req, res)  {
-    res.sendFile(path.join(__dirname, './views/about.html'));
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, './views/about.html'));
+});
+
+app.get('/employee/add', function (req, res) {
+  res.sendFile(path.join(__dirname, './views/addEmployee.html'));
+});
+
+app.get('/images/add', function (req, res) {
+  res.sendFile(path.join(__dirname, './views/addImage.html'));
 });
 
 //Employees
-app.get('/employees', function(req, res){
-dataservice.getAllEmployees().then((data)=>{
-    res.json(data);
-}
-)});
+app.get('/employees', function (req, res) {
+  if (req.query.department) {
+    dataservice.getEmployeesByDepartment(req.query.department).then((dataservice) => {
+      res.json(dataservice);
+    });
 
-// Managers
-app.get('/managers', function(req, res){
-    dataservice.getAllManagers().then((data)=>{
-        res.json(data);
+  }
+  else if (req.query.employeeNum) {
+    dataservice.getEmployeeByNum(req.query.employeeNum).then((dataservice) => {
+      res.json(dataservice);
+    });
+  }
+  else if (req.query.status) {
+    dataservice.getEmployeesByStatus(req.query.status).then((dataservice) => {
+      res.json(dataservice);
+    });
+  }
+  else if (req.query.manager) {
+    dataservice.getEmployeesByManager(req.query.manager).then((dataservice) => {
+      res.json(dataservice);
+    });
+  }
+
+  else {
+    dataservice.getAllEmployees().then((dataservice) => {
+      res.json(dataservice);
+    });
+
+  }
+});
+
+app.get("/images", function (req, res) {
+  fs.readdir("./public/images/uploaded", (err, items) => {
+    for (let i = 0; i < items.length; i++) {
+      items[i];
     }
-    )});
+    return res.json({ images: items });
+  })
+
+});
+
+app.get("/employee/:empNum", (req, res) => {
+  dataservice.getEmployeeByNum(req.params.empNum).then((dataservice) => {
+    res.json(dataservice);
+  });
+
+});
+
+//POST ROUTE (REDIREDT TO ROUTE /images)
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+  res.redirect("/images");
+});
+
+//POST ROUTE (REDIREDT TO ROUTE /employees)
+app.post("/employees/add", (req, res) => {
+  dataservice.addEmployee(req.body).then((dataservice) =>{
+      res.redirect("/employees");
+  });
+
+})
+// Managers
+app.get('/managers', function (req, res) {
+  dataservice.getAllManagers().then((dataservice) => {
+    res.json(dataservice);
+  }
+  )
+});
 
 // Department
-app.get('/departments', function(req, res){
-    dataservice.getAllDepartments().then((data)=>{
-        res.json(data);
-    }
-    )});
+app.get('/departments', function (req, res) {
+  dataservice.getAllDepartments().then((dataservice) => {
+    res.json(dataservice);
+  }
+  )
+});
 
 // Here I have added a new error page which will show the 404 Error 
-    app.use(function (req, res) {
-        res.sendFile(path.join(__dirname,'/views/error.html'));
-      });
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/error.html'));
+});
 
 dataservice
   .initialize()
